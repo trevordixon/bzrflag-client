386d4e8aba0c7e72ab3f36a881a633d9fca80730
brismuth <brismuth@gmail.com>
2014-05-05 14:40:29 -0600

    Tanks now move back to base after getting flag.

--- agents/pfa.js
+++ agents/pfa.js
@@ -23,24 +23,33 @@ if (process.argv.length > 2) {
 	process.exit();
 }
 
-var constants, bases, obstacles, staticFields;
+var constants, bases, obstacles, staticFields,  myBaseField, myColor;
 
 init(continueInit);
 function onUpdate(state) {
 	var myTanks = state[0];
 	var flags = state[1];
 	var fields = staticFields.slice(0);
+
 	var i;
 	for (i = 0; i < flags.length; ++i) {
-		if (flags[i]['color'] !== constants['team'] && flags[i]['possessionColor'] !== constants['team']) {// not my flag and not in my possession
+		if (flags[i]['color'] !== myColor && flags[i]['possessionColor'] !== myColor) {// not my flag and not in my possession
 			fields.push(new Field([flags[i]['loc']['x'], flags[i]['loc']['y']], constants['flagradius'], 900, 'seek', 5));
 		}
 	}
 
-	var t = 0;
-	for (t = 0; t < myTanks.length; ++t) {
-		var gradient = pf.gradient([myTanks[t]['loc']['x'], myTanks[t]['loc']['y']], fields);
-		console.log(gradient);
+	for (i = 0; i < myTanks.length; ++i) {
+		var gradient = pf.gradient([myTanks[i]['loc']['x'], myTanks[i]['loc']['y']], fields);
+		if (myTanks[i]['flag'] !== '-') {//our tank has a flag!!
+			//fields.push(myBaseField);
+			console.log('I haz flag! Going home.');
+		}
+
+		var position = {
+			"x": myTanks[i]['loc']['x'] + gradient[0],
+			"y": myTanks[i]['loc']['y'] + gradient[1]
+		}
+		moveToPosition(myTanks[i], position);
 	}
 
 	console.log('updated instructions');
@@ -71,6 +80,15 @@ function init(cb) {
 
 function continueInit() {
 	initStaticFields();
+	myColor = constants['team'];
+	var i;
+	for (i = 0; i < bases.length; ++i) {
+		if (bases[i]['color'] === myColor) {
+			var myBase = bases[i];
+			// put myBaseField = {} here
+
+		}
+	}
 	updateContinously();
 }
 
@@ -106,3 +124,15 @@ function updateContinously() {
 		}
 	);
 }
+
+
+
+
+function moveToPosition(tank, pos, callback) {
+	var angle = Math.atan2(pos.y-tank.loc.y,pos.x-tank.loc.x);
+	var relativeAngle = Math.atan2(Math.sin(angle - tank.angle), Math.cos(angle - tank.angle));
+	var distance = Math.sqrt(Math.pow(pos.x-tank.loc.x,2)+Math.pow(pos.y-tank.loc.y,2));
+	client.speed(tank.index, Math.min(distance/60,1));
+	client.angvel(tank.index, relativeAngle/2, callback);
+	client.shoot(tank.index);
+};
